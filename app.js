f9b2d1f124930d43ea71ec912d1d176525896726
ucurtmaavcısı <simsekmerve1717@gmail.com>
2022-02-16 08:21:08 +0300

    bug-fix

--- app.js
+++ app.js
@@ -3,19 +3,24 @@ let input2=document.querySelector("#second");
 
 let check=document.querySelector("#check")
 let option=document.querySelector("#option")
+let bottom=document.querySelector(".bottom")
 let result=0;
 
 check.addEventListener("click",()=>{
     if(option.value =="+"){
-        result+=Number(input1.value) + Number(input2.value)
-        alert(result);
+        result=Number(input1.value) + Number(input2.value)
+       bottom.innerText=result;
+       result=0;
     }else if(option.value =="-"){
         result+=Number(input1.value) - Number(input2.value)
-        alert(result);
+        bottom.innerText=result;
+        result=0;
 }else if(option.value =="*"){
     result+=Number(input1.value) * Number(input2.value)
-    alert(result);}
+    bottom.innerText=result;
+    result=0;}
     else if(option.value =="/"){
         result+=Number(input1.value) / Number(input2.value)
-        alert(result);}
+        bottom.innerText=result;
+        result=0;}
 });
